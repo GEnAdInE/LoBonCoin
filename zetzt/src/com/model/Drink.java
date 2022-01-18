c7b08b2746d85c6e919cf5ef468fca00b1c84ed6
Papy92 <clement.moreau92@gmail.com>
2022-01-18 11:30:02 +0100

    Classes

---
+++ zetzt/src/com/model/Drink.java
@@ -0,0 +1,47 @@
+package com.model;
+
+public class Drink 
+{
+	private String name;
+	
+	private String description;
+	
+	private float price;
+
+	public Drink() {
+		super();
+	}
+
+	public Drink(String name, String description, float price) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
---
+++ zetzt/src/com/model/Water.java
@@ -0,0 +1,23 @@
+package com.model;
+
+public class Water 
+{
+private int quantity;
+	
+	public Water() {
+		super();
+	}
+
+	public Water(int quantity) {
+		super();
+		this.quantity = quantity;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
