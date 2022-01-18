00099659a91fb727164b90241e8040152c3dfed7
GEnAdInE <one.robin@outlook.fr>
2022-01-18 11:40:12 +0100

    Mockito working

--- test/com/test/MainTest.java
+++ test/com/test/MainTest.java
@@ -1,12 +1,32 @@
 package com.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class MainTest {
+
+    @InjectMocks
+    private Main underTest;
 
     @Test
     void main() {
+    }
+
+    @Nested
+    class WhenMaining {
+        @Mock
+        private String[] args;
+
+        @BeforeEach
+        void setup() {
+        }
     }
 }
